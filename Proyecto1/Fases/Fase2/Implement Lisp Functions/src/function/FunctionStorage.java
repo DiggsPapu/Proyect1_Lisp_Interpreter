2eec5c601f3aa84ec74ab1b0266965e7e249e12d
Your Name <alo20172@uvg.edu.gt>
2022-02-24 10:40:33 -0600

    -Function(Storage&Reader)

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/FunctionStorage.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/FunctionStorage.java
@@ -1,5 +1,24 @@
 package function;
 
+import java.util.Stack;
+import java.util.HashMap;
 public class FunctionStorage {
+	HashMap functionS;
+	
+	public void storageNewFunction(String functionName, String[] arrayInstructions) {
+		/**
+		 * To replace a function
+		 */
+		if (functionS.containsKey(functionName)) {
+			functionS.replace(functionName, functionS.get(functionName), arrayInstructions);
+		}
+		/**
+		 * To create a function
+		 */
+		else {
+			functionS.put(functionName, arrayInstructions);
+		}
+		
+	}
 
 }
--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Reader.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Reader.java
@@ -1,15 +1,73 @@
 package function;
 import java.util.regex.Pattern;
+import java.util.Stack;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 
 public class Reader {
+	FunctionStorage functionStorage;
+	FunctionStorage variableStorage;
+	
+	
 	public void caseReader(String scan) {
+		switch (getCase(scan)) {
+		
+		case "function":{
+			/**
+			 * @param scanName 
+			 * @param Instructions
+			 * El nombre de la funcion y el set de instrucciones en array
+			 */
+			System.out.print("Entro en el function case\n");
+//			System.out.print(splitingExpresion(scan)[4]);
+//			functionStorage.storageNewFunction(splitingExpresion(scan)[4], splitingExpresion(scan));
+//			
+		}
+		
+		case "variable":{
 			
+		}
+
+		case "exp":{
+			
+		}
+		
+		
 	}
-	private String getCase(String scan) {
-		if (evaluate("^[((][]"), scan) {
+	}
+	/**
+	 * 
+	 * @param regularExpresion
+	 * @param expresion
+	 * @return boolean
+	 */
+	private boolean evaluateExpresion(String regularExpresion, String expresion) {
+		Pattern exp = Pattern.compile(regularExpresion, Pattern.CASE_INSENSITIVE);
+		Matcher match = exp.matcher(expresion);
+		System.out.print("Ingresa a evaluar la expresion\n");
+		return match.find();
+	}
+	
+	private String getCase(String scann) {
+		if (evaluateExpresion("^[(][ ]*defun[ ]+[[A-Za-z][0-9]*]+[)]$", scann)) {
+			System.out.print("Reconoce el case\n");
 			return "function";
 		}
-	}
+			
+		else if (evaluateExpresion("^[a-z]+@uvg.edu.gt$",scann)) //This is a professor
+			return "variable";
+		else if (evaluateExpresion("^[a-z]+_adm@uvg.edu.gt$",scann)) //This is an employee
+			return "exp";
+		else {
+			return "none";
+		}
 
+	}
+	
+	
+	private String[] splitingExpresion(String theScan) {
+		String[] functionArray = theScan.split("^[(]*[ ]defun*[ ]+[[a-zA-Z]*[a-zA-Z0-9_]]+[ ][(][a-z][)][)]$");
+		return functionArray;
+	}
+	
 }
