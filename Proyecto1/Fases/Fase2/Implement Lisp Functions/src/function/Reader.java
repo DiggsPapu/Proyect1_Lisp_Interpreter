c60bd951a785722d5be088328611e07c0c41a14f
AleMar21430 <99276653+AleMar21430@users.noreply.github.com>
2022-03-24 13:06:00 -0600

    Update Reader.java

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Reader.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Reader.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
+
 public class Reader {
 	private FunctionStorage functionStorage;
 	private VariableStorage variableStorage;
@@ -14,7 +15,8 @@ public class Reader {
 	protected static final String LITERAL = "[a-zA-Z0-9]+";
 	protected static final String VALID_NAME = "[a-zA-Z][a-zA-Z0-9]*";
 	protected static final String WHIESPACE = "[\\s]+";
-	protected static final String NUMERIC_ATOM = "[\\d\\+\\-\\*\\/\\^]?[\\d]*";
+	protected static final String NUMERIC_ATOM = "[\\+\\-]?[\\d]+[\\.\\d]*";
+	protected static final String OPERATIONS =  "[\\+\\-\\*\\^\\/]?";
 	protected static final String SYMBOL = "[().]";
 	protected static final String QUOTATION = "[\"]";
 	protected static final String ATOM = "ATOM";
@@ -24,7 +26,14 @@ public class Reader {
 	protected static final String DEFUN = "DEFUN";
 	protected static final String COND = "COND";
 	protected static final String EQUAL = "EQUAL";
+	protected static final String MAYOR_MINUS = "[<>.]";
 	
+	public boolean Mayor(String scan) {
+		if (scan.matches(MAYOR_MINUS)) {
+			return true;
+		}
+		return false;
+	}
 	
 	public FunctionStorage getFunctionStorage() {
 		return functionStorage;
@@ -41,13 +50,18 @@ public class Reader {
 	public void setVariableStorage(VariableStorage variableStorage) {
 		this.variableStorage = variableStorage;
 	}
-
+	
+	public Reader() {
+		this.functionStorage = new FunctionStorage();
+		this.variableStorage = new VariableStorage();
+	}
+	
+	
 	/**
 	 * It divides the text in valid tokens
 	 * @param command
 	 * @return
 	 */
-	
 	
 	public LinkedList <String> tokenize(String command){
 		int i = 0;
@@ -58,7 +72,7 @@ public class Reader {
 		}
 		while (  i < command.length() ){
 			int j = i + 1;
-			if ( command.substring(i, j).matches(LETTER) || command.substring(i, j).matches(NUMERIC_ATOM)||command.substring(i, j).matches(QUOTATION) ){
+			if ( command.substring(i, j).matches(LETTER) || command.substring(i, j).matches(NUMERIC_ATOM)||command.substring(i, j).matches(QUOTATION)||command.substring(i, j).matches(OPERATIONS) ){
 				while ( command.substring(i,j + 1).matches(LITERAL) || command.substring(i, j + 1).matches(NUMERIC_ATOM) ){
 					j++;
 				}
@@ -68,34 +82,11 @@ public class Reader {
 			}
 			i = j;
 		}
-		System.out.print(tokens.size());
+		System.out.print("tOKEN SIZE:"+ String.valueOf(tokens.size())+"\n" );
 		return tokens;
 	}
 	
-	public Integer getCase(LinkedList<String> lista) {
-		System.out.println(lista);
-                Arithmetic_Operations AMOP = new Arithmetic_Operations(lista);
-                System.out.println(AMOP.Result());
-		if (QUOTE.equals(lista.get(1).trim())) {
-			return 1;
-		}else if (DEFUN.equals(lista.get(1))) {
-			return 2;
-		}else if (SETQ.equals(lista.get(1))) {
-			return 3;
-		}else if (ATOM.equals(lista.get(1))) {
-			return 4;
-			
-		}else if (LIST.equals(lista.get(1))) {
-			return 5;
-			
-		}else if (EQUAL.equals(lista.get(1))) {
-			return 6;
-		}else if (COND.equals(lista.get(1))) {
-			return 7;
-		}else {
-			return (Integer) null;
-		}
-	}
+	
 	
 	
 	
@@ -111,47 +102,266 @@ public class Reader {
 			break;
 		}
 		case 2: {
-			
+			break;
 		}
 		case 3:{
-			
+			System.out.print("Entro a setq\n");
+			caseSETQ(array);
+			break;
 		}
 		case 4:{
-			
+			break;
+		}
+		case 5:{
+			caseList(array);
+			break;
+		}
+		case 6:{
+//			caseEqual(array);
+			break;
+		}
+		case 8:{
+			//En el caso de que la operacion no se pudiera realizar
+			System.out.print("La operacion no pudo realizarse\n");
+			break;
+		}
+		case 9:{
+			break;
 		}
 		default:{
-			
+			break;
 		}
-//		
-//		case "variable":{
-//			
-//			
-//		}
-//
-//		case "exp":{
-//			
-//		}
-//		
-//		
-	}
+		
+		}
 	}
 	
+	
+	
+	
+	public Integer getCase(LinkedList<String> lista) {
+		System.out.println(lista);
+		
+		if (QUOTE.equals(lista.get(1).trim())) {
+			return 1;
+		}else if (DEFUN.equals(lista.get(1))) {
+			return 2;
+		}else if (SETQ.equals(lista.get(1))) {
+			return 3;
+		}else if (ATOM.equals(lista.get(1))) {
+			return 4;
+			
+		}else if (LIST.equals(lista.get(1))) {
+			System.out.print("Ingreso a list\n");
+			return 5;
+			
+		}else if (EQUAL.equals(lista.get(1))) {
+			return 6;
+		}else if (COND.equals(lista.get(1))) {
+			return 7;
+		}else if(lista.get(1).matches(OPERATIONS)||lista.get(1).equals("(")) {
+			if (caseOperation(lista)==null) {
+				return 8;
+			}
+			else {
+				Arithmetic_Operations calc = new Arithmetic_Operations(caseOperation(lista));
+				System.out.print(calc.Result());
+				return 9;
+			}
+		}else if(lista.get(1).matches(MAYOR_MINUS)) {
+                        Comparator COMP = new Comparator(caseOperation(lista));
+                    System.out.print(COMP.Result());
+			return 10;
+		}
+		
+		
+		else {
+			return (Integer) null;
+		}
+	}
+	
+//	public boolean caseMayor_Minus(LinkedList<String> lista) {
+//		if ()
+//	}
+	
+	
+	
+	public boolean caseEqual(LinkedList<String> lista) {
+		//En caso cumpla con la sintaxis
+		if (lista.get(2).equals("*") && lista.get(4).equals("*") && lista.get(lista.size()-2).equals("*") && lista.get(lista.size()-4).equals("*")) {
+			System.out.print("Primer if\n");
+			
+			
+			
+			//En caso de que algun valor a comparar sea una variable
+			if (lista.get(3).matches(VALID_NAME) && getVariableStorage().getVariableStorage().containsKey(lista.get(3)) || lista.get(lista.size()-3).matches(VALID_NAME) && getVariableStorage().getVariableStorage().containsKey(lista.get(lista.size()-3))  ) {
+				System.out.print("Segundo if true\n");
+				//En caso de que sean verdaderos
+				if (lista.get(3).equals(lista.get(lista.size()-3)))
+				//En caso de que sea verdadera la igualdad
+				System.out.print(true);
+			}
+			else {
+				System.out.print(true);
+				//En dado caso sea falsa la igualdad
+			}
+			//En dado caso cumpla con la sintaxis
+			return true;
+		}
+		
+		
+		
+		//En caso de que no cumpla con la sintaxis
+		else {
+			return (Boolean) null;
+		}
+	}
+	
+	
+	public LinkedList<String> caseList(LinkedList<String> lista){
+		LinkedList<String> list = new LinkedList<String>();
+		if (lista.get(0).equals("(") && lista.get(lista.size()-1).equals(")")) {
+			System.out.print("Primer if de linked list\n");
+			for(int k = 2 ; k < lista.size()-1 ; k++ ) {
+				System.out.print("List value actual: "+ lista.get(k)+"\n");
+				//En el caso de que sea un string definido entre comillas
+				if (lista.get(k).matches(QUOTATION)) {
+					k++;
+					String tempString = new String();
+					//En el caso de que sea un string, definido entre comillas
+					while (!lista.get(k).matches(QUOTATION) && k < lista.size()-1) {
+						//Se concatena
+						tempString = tempString + " " + lista.get(k);
+						//Significa que llego al final de la lista y concateno el parentesis cerrado, mal escrito entonces se retorna null.
+						if (k==lista.size()-2) {
+							return null;
+						}
+						//Se sube el contador
+						k++;
+					}
+					//Si se mantuvo entonces se add the string
+					list.add(tempString);
+					
+				
+				}
+				//En el caso cumpla con el patron de ser numerico entonces se add
+				else if (lista.get(k).matches(NUMERIC_ATOM)){
+					list.add(lista.get(k));
+				}
+				//En el caso de que sea un nombre valido y que exista en el almacenamiento de variables se busca y se add
+				else if (lista.get(k).matches(VALID_NAME) && getVariableStorage().getVariableStorage().containsKey(lista.get(k))) {
+					list.add(getVariableStorage().getVariableStorage().get(lista.get(k)));
+				}
+				//En el caso de que tenga o no un nombre valido y no este almacenado en el almacenamiento de variables
+				else if (lista.get(k).matches(VALID_NAME) || lista.get(k).matches(VALID_NAME) && !getVariableStorage().getVariableStorage().containsKey(lista.get(k))) {
+					return null;
+				}
+				else {
+					
+				}
+				
+			}
+			System.out.print("La lista final"+list+"\n");
+			return list;
+		}
+		//En el caso de que no cumpla con que tenga parentesis se devuelve nulo
+		return null;
+	}
+	
+	
+	
+	
+	public LinkedList<String> caseOperation(LinkedList<String> lista) {
+		int oparenthesisCounter = 0;
+		int cparenthesisCounter = 0;
+		
+		//Para correr la lista de operaciones
+		for (int k = 0; k < lista.size() ; k++) {
+			System.out.print(lista.get(k));
+			//Si es una variable almacenada
+			if (getVariableStorage().getVariableStorage().containsKey(lista.get(k))) {
+				try {
+					//Trata de convertirlo a numero float
+					Float.parseFloat(getVariableStorage().getVariableStorage().get(lista.get(k)));
+					//Si es numero se agrega y suplementa al valor que aparecia
+					lista.add(k, getVariableStorage().getVariableStorage().get(lista.get(k)));
+					lista.remove(k+1);
+					
+				}
+				//En el caso de que no sea numero significa que es error del usuario
+				catch(Exception e) {
+					System.out.print("No es numero la variable\n");
+					return null;
+				}
+				
+			}
+			//En el caso de que sea igual a parentesis abierto 
+			else if (lista.get(k).equals("(")) {
+				oparenthesisCounter++;
+				//Si difiere en ser el siguiente valor operaciones.
+				if (lista.get(k+1).matches(OPERATIONS)) {
+					
+				}
+				//Si difiere en ser el siguiente valor parentesis abierto
+				else if(lista.get(k+1).equals("(")) {
+				}
+				else {
+					System.out.print("El siguiente valor no es operaciones o parentesis abierto\n");
+					return null;
+				}
+				
+			}
+			else if (lista.get(k).equals(")")) {
+				System.out.print("Entro a parentesis cerrado\n");
+				cparenthesisCounter++;
+			}
+			//En caso de que no sea una key de variable es error de usuario
+			else if (lista.get(k).matches(VALID_NAME) && !getVariableStorage().getVariableStorage().containsKey(lista.get(k))|| lista.get(k).matches(QUOTATION)) {
+				System.out.print("no es key valida\n");
+				return null;
+				
+			//En caso de que sea una operacion y el anterior no sea parentesis abierto es error	
+			}else if(lista.get(k).matches(OPERATIONS) && !lista.get(k-1).equals("(")) {
+				System.out.print("El anterior no es parentesis abierto\n");
+				return null;
+			}
+			
+		}
+		//No balanceados los parentesis
+		if (cparenthesisCounter!=oparenthesisCounter) {
+			System.out.print("\n"+cparenthesisCounter+" "+oparenthesisCounter);
+			return null;
+		}
+		//Si pasa todo esto entonces retornara la lista
+		return lista;
+	}
+	
+//	.substring(0,array.get(array.size()-1).length()-1)
 	public void caseSETQ(LinkedList<String> array) {
-		if (VALID_NAME.equals(array.get(2))) {
+//		System.out.print(array.get(2).substring(0, array.get(2).length()-1).matches(VALID_NAME) && array.get(array.size()-1).matches(SYMBOL) );
+		if (array.get(2).substring(0, array.get(2).length()-1).matches(VALID_NAME) && array.get(array.size()-1).matches(SYMBOL) ) {
 			// Para verificar que es valida 
-			if (SYMBOL.equals(array.get(3)) && SYMBOL.equals(array.get(array.size()-2)) && QUOTATION.equals(array.get(4)) &&  QUOTATION.equals(array.get(array.size()-3))) {
+			System.out.print("Primer if\n");
+//			System.out.print(array.get(3).matches(SYMBOL) && array.get(array.size()-2).matches(SYMBOL) && array.get(4).matches(QUOTATION) &&  array.get(array.size()-3).matches(QUOTATION));
+			if ( array.get(3).matches(SYMBOL) && array.get(array.size()-2).matches(SYMBOL) && array.get(4).matches(QUOTATION) &&  array.get(array.size()-3).matches(QUOTATION) ) {
+				System.out.print("SegundoIf\n");
 				// Es para almacenar el valor tipo string
 				String value = new String();
 				//Para verificar que es valida la concatenacion
 				boolean valido = true;
 				//Para correr el array
-				for (int k = 4 ; k < array.size()-3; k++ ) {
-					if (!array.get(k).equals(QUOTATION) && valido) {
-						value.concat(array.get(k));
-						value.concat(" ");
-						if (k == array.size()-3) {
+				for (int k = 5 ; k < array.size()-3; k++ ) {
+					//El if es para verificar si hay una quotation o si es valido
+					//Si hay un " entonces significa que no es valido y pasa al else para no almacenar mas valores.
+					if (!array.get(k).matches(QUOTATION) && valido) {
+						System.out.print("3er if\n");
+						//Se concatena el string
+						value=value+" "+array.get(k);
+						if (k == array.size()-4) {
 							value.trim();
+							System.out.print("Final if\n");
+							
 						}
+						System.out.print("VALOR: "+value+"\n");
 					}
 					else {
 						valido = false;
@@ -159,37 +369,61 @@ public class Reader {
 					
 				}
 				if (valido) {
+					//Es para almacenar el valor de la variable en el variable Storage
 					getVariableStorage().CreateVariable(array.get(2), value);
+				}else {
+					System.out.print("No es valida la sintaxis 2\n");
 				}
 				
 				
+			}else if(array.get(3).matches(SYMBOL) && array.get(array.size()-2).matches(SYMBOL) && array.get(4).matches(OPERATIONS)) {
+				//Modificar la lista para que se adapte al caso de operaciones
+				System.out.print("Entro a variable numerica en setq\n");
+				LinkedList<String> operationsArray = new LinkedList<String>();
+				for (int k = 3 ; k < array.size()-1 ; k++) {
+					operationsArray.add(array.get(k));
+				}
+				if (caseOperation(operationsArray)!=null) {
+					Arithmetic_Operations calc = new Arithmetic_Operations(caseOperation(operationsArray));
+					System.out.print(operationsArray);
+					getVariableStorage().CreateVariable(array.get(2), String.valueOf(calc.Result()));
+					System.out.print("El valor de la variable es: "+getVariableStorage().getVariableStorage().get(array.get(2))+"\n");
+				}
+				else {
+					System.out.print("No era valida la expresion\n");
+				}
+			}
+			else if(array.get(4).matches(NUMERIC_ATOM) && array.get(5).equals(")") && array.get(6).equals(")")) {
+				try {
+					Float.valueOf(array.get(4));
+					getVariableStorage().getVariableStorage().put(array.get(2), array.get(4));
+					System.out.print("El valor es: " + getVariableStorage().getVariableStorage().get(array.get(2))+"\n");
+				}
+				catch(Exception e) {
+					System.out.print("No era un valor valido\n");
+				}
 			}
 			else {
-				System.out.print("No es una sintaxis valida");
+				System.out.print("No es una sintaxis valida\n");
 			}
 		}else {
-			System.out.print("No es valida la sintaxis.");
+			System.out.print("No es valida la sintaxis.\n");
 		}
 	}
 	
 	
-	
-	private String[] splitingExpresion(String theScan) {
-		String[] functionArray = theScan.split("[(][ ]defun*");
-		for (int k=0;k<functionArray.length;k++) {
-			System.out.print(functionArray[k]+"\n");
-		}
-		return functionArray;
-	}
 	
 	public static void main(String[] args) {
 		Reader lector = new Reader();
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println(lector.getCase(lector.tokenize(scanner.nextLine())));
+//		System.out.println(lector.getCase(lector.tokenize(scanner.nextLine())));
+//		lector.caseReader(scanner.nextLine());
+//		
 //		lector.caseReader(scanner.nextLine());
 
-		
+		System.out.print(lector.Mayor(scanner.nextLine()));
+		scanner.close();
 	}
 	
 }
