a6793bf6dcb1d94243c7d7b22008e4fbf6d52f9e
AleMar21430 <99276653+AleMar21430@users.noreply.github.com>
2022-03-24 21:56:40 -0600

    Update FunctionStorage.java

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/FunctionStorage.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/FunctionStorage.java
@@ -1,22 +1,18 @@
 package function;
 
-import java.util.Stack;
 import java.util.HashMap;
+
 public class FunctionStorage {
-	HashMap functionS;
-	
-	public void storageNewFunction(String functionName, String[] arrayInstructions) {
-		/**
-		 * To replace a function
-		 */
-		if (functionS.containsKey(functionName)) {
-			functionS.replace(functionName, functionS.get(functionName), arrayInstructions);
-		}
-		/**
-		 * To create a function
-		 */
-		else {
-			functionS.put(functionName, arrayInstructions);
-		}
-	}
+    HashMap Function;
+
+    public void storageNewFunction(String functionName, String[] arrayInstructions) {
+
+        if (Function.containsKey(functionName)) {
+                Function.replace(functionName, Function.get(functionName), arrayInstructions);
+        }
+
+        else {
+                Function.put(functionName, arrayInstructions);
+        }
+    }
 }
