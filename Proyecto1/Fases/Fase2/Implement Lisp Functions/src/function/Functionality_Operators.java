0070c90adfef67d92ed99d3cd1b9e1faaa09d1ce
AleMar21430 <99276653+AleMar21430@users.noreply.github.com>
2022-03-24 19:01:38 -0600

    Update Functionality_Operators.java

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Functionality_Operators.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Functionality_Operators.java
@@ -23,46 +23,45 @@ public class Functionality_Operators {
         if(list.get(1).equals("COND")&& list.get(0).equals("(") && list.get(list.size()-1).equals(")")) {
             LinkedList <String> Temp = new LinkedList <String>();
                 int Counter = 3;
-                while(!list.get(Counter).equals(")") ){
-                    Temp.add(list.get(Counter));
-                    Counter++ ;
-                }
+            while(!list.get(Counter).equals(")") ){
                 Temp.add(list.get(Counter));
-               
-            Operations OP = new Operations(Temp);
-            
-            if(OP.getOpType == ">"||){
+                Counter++ ;
+            }
+            Temp.add(list.get(Counter));
 
-                //Si la lista matches el patron de que sea un nombre valido entonces devolver el valor almacenado
-                if(list.get(Counter).matches("[a-zA-Z][a-zA-Z0-9]*") && variableStorage.getVariableStorage().containsKey(list.get(Counter))){
-                    System.out.print("El valor almacenado es: "+variableStorage.getVariableStorage().get(lista.get(Counter))+"\n");
-                    return variableStorage.getVariableStorage().get(lista.get(Counter)); //Retorna el valore almacenado del almacenamiento de variables
-                    
-                    
-                }else if(list.get(Counter).matches("[a-zA-Z][a-zA-Z0-9]*") && !variableStorage.getVariableStorage().containsKey(list.get(Counter))){
-                    System.out.print("Devolvio nulo");
-                    return null;
+        Operations OP = new Operations(Temp);
 
-                }else if (list.get(Counter).equals("(") && list.get(Counter+1).matches("[\\+\\-\\*\\^\\/]")){
-                    LinkedList <String> Temp1 = new LinkedList <String>();
-                    Operations tempOP = new Operations(Temp1);
-                    return Operations.Result();
-                    
-                                        
-                }else if (list.get(Counter).equals("(") && list.get(Counter+1).matches("[<>.]")){
-                    LinkedList <String> Temp1 = new LinkedList <String>();
-                    Operations tempOP = new Operations(Temp1);
-                    return Operations.ResultComp();
-                                 
-                }
-                
-        }else if (OP.getOpType == "<"){
-        
-        }else if(OP.getOpType == "="){
+        if(OP.getOpType() == ">"){
+
+            //Si la lista matches el patron de que sea un nombre valido entonces devolver el valor almacenado
+            if(list.get(Counter).matches("[a-zA-Z][a-zA-Z0-9]*") && variableStorage.getVariableStorage().containsKey(list.get(Counter))){
+                System.out.print("El valor almacenado es: "+variableStorage.getVariableStorage().get(list.get(Counter))+"\n");
+                return variableStorage.getVariableStorage().get(list.get(Counter)); //Retorna el valore almacenado del almacenamiento de variables
+
+
+            }else if(list.get(Counter).matches("[a-zA-Z][a-zA-Z0-9]*") && !variableStorage.getVariableStorage().containsKey(list.get(Counter))){
+                System.out.print("Devolvio nulo");
+                return null;
+
+            }else if (list.get(Counter).equals("(") && list.get(Counter+1).matches("[\\+\\-\\*\\^\\/]")){
+                LinkedList <String> Temp1 = new LinkedList <String>();
+                Operations tempOP = new Operations(Temp1);
+                //return Operations.Result();
+
+
+            }else if (list.get(Counter).equals("(") && list.get(Counter+1).matches("[<>.]")){
+                LinkedList <String> Temp1 = new LinkedList <String>();
+                Operations tempOP = new Operations(Temp1);
+                //return Operations.ResultComp();
+
+            }
+
+        }else if (OP.getOpType() == "<"){
+
+        }else if(OP.getOpType() == "="){
 
         }else{
             System.out.print("Error de Operando de comparación");      
         }
-        return list;
     }
 }
