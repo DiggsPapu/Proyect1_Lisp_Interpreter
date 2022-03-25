cc3ec328a87b4ae074e720b044c0f1aac02e230b
AleMar21430 <99276653+AleMar21430@users.noreply.github.com>
2022-03-24 23:58:53 -0600

    Le Atom et le Cons

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Functionality_Operators.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/Functionality_Operators.java
@@ -10,8 +10,9 @@ import java.util.LinkedList;
   
   
 public class Functionality_Operators {
-
+    
     LinkedList<String> Atom = new LinkedList<String>();
+    LinkedList<String> Cons = new LinkedList<String>();
     
     Functionality_Operators(){
         
@@ -26,60 +27,17 @@ public class Functionality_Operators {
     public LinkedList<String> getAtom(){
         return Atom;
     }
+    public void create_Cons(LinkedList<String> list){
+        if(list.size() != 5){
+            System.out.println("Error: CONS fue definido como ATOM (No debe tener mas o menos de dos variables)");
+        }else{
+            Cons = list;
+        }
+    }
+    public LinkedList<String> getCons(){
+        return Cons;
+    }
 }
 //ejemplo (ATOM 1 2 3 4 5)
 //ejemplo (ATOM 1)
 //ejemplo (ATOM 1 2)
-  
-
-    private FunctionStorage functionStorage;
-    private VariableStorage variableStorage;
-   
-   
-    
-    private static Object SaveOperands(LinkedList<String> list, FunctionStorage functionStorage, VariableStorage variableStorage){
-        if(list.get(1).equals("COND")&& list.get(0).equals("(") && list.get(list.size()-1).equals(")")) {
-            LinkedList <String> Temp = new LinkedList <String>();
-                int Counter = 3;
-            while(!list.get(Counter).equals(")") ){
-                Temp.add(list.get(Counter));
-                Counter++ ;
-            }
-            Temp.add(list.get(Counter));
-
-        Operations OP = new Operations(Temp);
-
-        if(OP.getOpType() == ">"){
-
-            //Si la lista matches el patron de que sea un nombre valido entonces devolver el valor almacenado
-            if(list.get(Counter).matches("[a-zA-Z][a-zA-Z0-9]*") && variableStorage.getVariableStorage().containsKey(list.get(Counter))){
-                System.out.print("El valor almacenado es: "+variableStorage.getVariableStorage().get(list.get(Counter))+"\n");
-                return variableStorage.getVariableStorage().get(list.get(Counter)); //Retorna el valore almacenado del almacenamiento de variables
-
-
-            }else if(list.get(Counter).matches("[a-zA-Z][a-zA-Z0-9]*") && !variableStorage.getVariableStorage().containsKey(list.get(Counter))){
-                System.out.print("Devolvio nulo");
-                return null;
-
-            }else if (list.get(Counter).equals("(") && list.get(Counter+1).matches("[\\+\\-\\*\\^\\/]")){
-                LinkedList <String> Temp1 = new LinkedList <String>();
-                Operations tempOP = new Operations(Temp1);
-                //return Operations.Result();
-
-
-            }else if (list.get(Counter).equals("(") && list.get(Counter+1).matches("[<>.]")){
-                LinkedList <String> Temp1 = new LinkedList <String>();
-                Operations tempOP = new Operations(Temp1);
-                //return Operations.ResultComp();
-
-            }
-
-        }else if (OP.getOpType() == "<"){
-
-        }else if(OP.getOpType() == "="){
-
-        }else{
-            System.out.print("Error de Operando de comparación");      
-        }
-    }
-}
