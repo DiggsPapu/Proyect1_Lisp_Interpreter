a58cbc02280b46fca93f9c0a2d44b30eedf8e7cc
Your Name <alo20172@uvg.edu.gt>
2022-03-25 21:43:01 -0600

    Ultimo commit de esta compu

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/tokenizer.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/function/tokenizer.java
@@ -26,7 +26,7 @@ public class tokenizer {
 				//In case de next substring matches de pattern it enters in the if to get the token
 				if (command.substring(i,j).matches(Patterns.LETTER) || command.substring(i, j).matches(Patterns.NUMERIC_ATOM) || command.substring(i, j).matches(Patterns.QUOTATION) || command.substring(i, j).matches(Patterns.OPERATIONS) ) {
 					//Mientras que el substring matches Literales o numericos entonces concatena todo lo que haya
-					while ( command.substring(i , j+1).matches(Patterns.LITERAL) || command.substring(i, j+1).matches(Patterns.NUMERIC_ATOM)) {
+					while ( command.substring(i , j+1).matches(Patterns.LITERAL) || command.substring(i, j+1).matches(Patterns.NUMERIC_ATOM) ) {
 						j++;
 					}
 					tokenList.add(command.substring(i, j).trim());
@@ -37,6 +37,7 @@ public class tokenizer {
 				}
 				i = j;
 			}
+//			System.out.print(tokenList);
 			return tokenList;
 		}else {
 			System.out.print("EXCEPTION no puso par de parentesis encerrando su funcion\n");
@@ -48,23 +49,72 @@ public class tokenizer {
 	 * @param command
 	 * @return lista, se retorna una lista con los tokens, sera tokenizada
 	 */
+	
+	private static LinkedList<String> quotation(LinkedList<String> lista) {
+		//Si la lista es diferente de nulo y contiene comillas
+		LinkedList<String> corrected = new LinkedList<String>();
+		//Si no es nula y contiene quotation
+		if (lista!= null && lista.contains("\"")) {
+			//Moverse a traves de la lista
+			for (int k = 0 ; k < lista.size() ; k++) {
+				//Si se encuentra un token que sea de quotation
+				if (lista.get(k).equals("\"")) {
+					k++;
+					
+					String quotation = new String();
+					while (!lista.get(k).equals("\"")) {
+						quotation = quotation+ " " + lista.get(k);
+						k++;
+					}
+					
+					corrected.add(quotation);
+				}else {
+					corrected.add(lista.get(k));
+				}
+				
+			}
+			
+		}else if (lista != null){
+			return lista;
+		
+		}else {
+			
+			return null;
+		}
+//		System.out.print("Corregido: "+corrected+"\n");
+		return corrected;
+	}
+
 	public static LinkedList<String> equalParenthesis(String command) {
 		//Asignar la lista tokenizada a una lista linkeada
 		LinkedList<String> tokenList = tokenizer.tokenized(command);
 		//Si es diferente de nulo entonces realiza el proceso
+		tokenList = tokenizer.quotation(tokenList);
+//		System.out.print(tokenList+"\n");
+		
+		//En el caso de que el que la lista sea diferente de nula
 		if (tokenList!= null) {
+			
+			//Se crean contadores para parentesis
 			int cop = 1;
 			int ccp = 0;
+			//Se recorre la lista para contar los parentesis
 			for (int k = 1 ; k < tokenList.size() ; k++ ) {
 //				System.out.print("Ingreso al for\n");
+				
+				//En el caso de que sea parentesis abierto
 				if ( tokenList.get(k).equals("(")) {
 					cop++;
 //					System.out.print("OP");
+					
+					
+				//En el caso de que sea parentesis cerrado
 				}else if( tokenList.get(k).equals(")")) {
 					ccp++;
 //					System.out.print("CP");
 				}
 			}
+			//Si son iguales entonces devuelve la lista, sino devuelve null
 			if (cop == ccp) {
 //				System.out.print(tokenList+"\n");
 				return tokenList;
@@ -72,12 +122,13 @@ public class tokenizer {
 				System.out.print("EXCEPTION no hay igualdad de parentesis\n");
 				return null;
 			}
+		
+		//Si la listaes nula entonces devuelve null
 		}else {
 			
 			return null;
 		}
 	}
-
 	
 //	public static void main(String[] args) {
 //		Scanner scanner = new Scanner(System.in);
--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/junit/testTokenizer.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/junit/testTokenizer.java
@@ -5,17 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import function.tokenizer;
 
+import java.util.Scanner;
 class testTokenizer {
 
 	@Test
 	void testEqualParenthesis() {
+		Scanner scann = new Scanner(System.in);
 		assertNotEquals(tokenizer.equalParenthesis("(setq 1 2 3)"), null);
 		assertEquals(tokenizer.equalParenthesis("(setq 1 2 3"), null);
 		assertEquals(tokenizer.equalParenthesis("(setq 1 2 (3)"), null);
 		assertEquals(tokenizer.equalParenthesis("fjkl"), null);
 		assertEquals(tokenizer.equalParenthesis("exit"), null);
 		assertEquals(tokenizer.equalParenthesis("e"), null);
-		
+		System.out.print("Ingresar un input que contenga comillas: ");
+		assertNotEquals(tokenizer.equalParenthesis(scann.nextLine()), null);
+		scann.close();
 	}
 
 }
