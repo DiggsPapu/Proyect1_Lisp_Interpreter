4469fe1a55700e6924af56c02b1d51d170868650
Your Name <alo20172@uvg.edu.gt>
2022-03-27 23:17:15 -0600

    jk

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/junit/testTokenizer.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/junit/testTokenizer.java
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-package junit;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
-import v2.tokenizer;
-
-import java.util.Scanner;
-class testTokenizer {
-
-	@Test
-	void testEqualParenthesis() {
-		Scanner scann = new Scanner(System.in);
-		assertNotEquals(tokenizer.equalParenthesis("(setq 1 2 3)"), null);
-		assertEquals(tokenizer.equalParenthesis("(setq 1 2 3"), null);
-		assertEquals(tokenizer.equalParenthesis("(setq 1 2 (3)"), null);
-		assertEquals(tokenizer.equalParenthesis("fjkl"), null);
-		assertEquals(tokenizer.equalParenthesis("exit"), null);
-		assertEquals(tokenizer.equalParenthesis("e"), null);
-		System.out.print("Ingresar un input que contenga comillas balanceadas: "); //(setq fkdls "fjksdl fdsa")
-		assertNotEquals(tokenizer.equalParenthesis("(value())"), null);
-		System.out.print("Ingresar un input que contenga comillas desbalanceadas: "); //(setq fkdls "fjksdl )
-		assertNull(tokenizer.equalParenthesis("(value ()"));
-		scann.close();
-	}
-
-}
-=======
 package junit;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,4 +26,3 @@ class testTokenizer {
 	}
 
 }
->>>>>>> 6f24e712079cd755ef2d83c702f6cb1680e0df3c
