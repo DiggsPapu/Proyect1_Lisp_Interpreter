c93bac43a73a39327a01af3f26db74ce9edb96e7
AleMar21430 <99276653+AleMar21430@users.noreply.github.com>
2022-03-27 22:58:13 -0600

    Update VariableStorage.java

--- Proyecto1/Fases/Fase2/Implement Lisp Functions/src/v2/VariableStorage.java
+++ Proyecto1/Fases/Fase2/Implement Lisp Functions/src/v2/VariableStorage.java
@@ -2,19 +2,33 @@ package v2;
 
 
 import java.util.TreeMap;
+/**
+* Este metodo constructor para generar las operaciones
+*/
 public class VariableStorage {
 	private TreeMap<String, String> variableStorage;
-
-
-
+/**
+* Este metodo devuelve la variable almacenada.
+* @return Treemap con las variables guardadas
+*/
 	public TreeMap<String, String> getVariableStorage() {
 		return variableStorage;
 	}
 
+/**
+* Este metodo crea el Treemap para guardar las variables
+* @return None
+*/	
+
 	public VariableStorage(){
 		variableStorage = new TreeMap<>();
 	}
-
+/**
+* Con este metodo se almacenan las variables
+* @param value
+* @param name
+* @return None
+*/
 	public void CreateVariable(String name, String value) {
 		variableStorage.put(name, value);
 	}
